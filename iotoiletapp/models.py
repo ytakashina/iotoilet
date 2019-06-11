06afa0612c965193ef2b710c31c5ce2d2d97dd59
k-nakamu <51604342+k-nakamu@users.noreply.github.com>
2019-06-11 16:45:26 +0900

    性別を追加

--- iotoiletapp/models.py
+++ iotoiletapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Sex(models.Model):
+    sex_name = models.CharField("性別名", max_length=20)
+    sex_no = models.IntegerField("性別", default=1)
+  
+    def __str__(self):
+        return self.sex_name
