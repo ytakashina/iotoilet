a1506ad3452cdadcfdde73584a3f4e1036d5c589
MKIMA <50836020+TomoeAsagami@users.noreply.github.com>
2019-06-11 16:43:46 +0900

    クラスFloor, Userを追加

--- iotoiletapp/models.py
+++ iotoiletapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Floor(models.Model):
+    floor_name = (
+        (1, "25階"),
+        (2, "26階"),
+        (3, "27階"),
+        (4, "28階"),
+        (5, "29階"),
+    )
+    floor_no = models.IntegerField("フロアNo.")
+
+class User(models.Model):
+    sex = models.ForeignKey('Sex', on_delete=models.CASCADE)
+    floor_id = models.ForeignKey('Floor', on_delete=models.CASCADE)
+    user_id = models.CharField("ユーザID", max_length=20)
+    password = models.CharField("パスワード", max_length=20)
+    user_name = models.CharField("ユーザ名", max_length=20)
+
+    def __str__(self):
+        return self.user_name
