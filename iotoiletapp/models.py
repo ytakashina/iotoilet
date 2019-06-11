5659f75bf8141c076bd76a17db2ba75328be19d4
daisukefuku <51604212+daisukefuku@users.noreply.github.com>
2019-06-11 16:51:45 +0900

    Update models.py

--- iotoiletapp/models.py
+++ iotoiletapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class ToiletStatusType(models.Model):
+
+    toilet_status_type_id = models.CharField("個室状況ID", max_length=20)
+    toilet_status_type_name = models.CharField("個室状況名称", max_length=100)
+    toilet_status_type_name = models.IntegerField("個室状況NO", default=-1)
+
+    def __str__(self):
+        return self.toilet_status_type_id
+
+
+class ToiletStatus(models.Model):
+
+    timestamp = models.DateTimeField("登録時刻")
+    toilet_id = models.CharField("個室ID", max_length=20)
+    toilet_status_type_id = models.CharField("個室状況ID", max_length=20)
+
+    def __str__(self):
+        return [self.timestamp, self.toilet_id]
+
