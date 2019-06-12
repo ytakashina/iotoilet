0646de2114eed80eedc0bfa870519f3c527f9f78
daisukefuku <51604212+daisukefuku@users.noreply.github.com>
2019-06-12 14:41:13 +0900

    Update urls.py

--- iotoilet/urls.py
+++ iotoilet/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    # path('iotoiletapp/', include('iotoiletapp.urls')),
+    path('iotoiletapp/', include('iotoiletapp.urls')),
     # path('iotoiletapi/', include('iotoiletapi.urls')),
     path('admin/', admin.site.urls),
 ]
