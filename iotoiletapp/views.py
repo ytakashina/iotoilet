f6a125b1c2e5e6d47601c20b3e1edcad48ccfd26
MKIMA <50836020+TomoeAsagami@users.noreply.github.com>
2019-06-12 13:59:16 +0900

    views

--- iotoiletapp/views.py
+++ iotoiletapp/views.py
@@ -2,25 +2,33 @@ from django.shortcuts import render, get_object_or_404, redirect #追加
 from django.http import HttpResponse
 from iotoiletapp.models import Toilet, Room, ToiletStatus
 from iotoiletapp.forms import IotoiletappForm
+import datetime
+import psycopg2
+
+CONN = "host=ytakashina.cp12crxu8zls.ap-northeast-1.rds.amazonaws.com" +\
+" port=5432 dbname=postgres user=postgres password=postgres"
 
 #一覧
 def index(request):
-    toilet_list = CONNECTION.objects.raw('select  T.toilet_name, ' +\
-    'RT.room_type_name, F.floor_name, TST.toilet_status_type_name, SD.timestamp, SD.value1' +\
-    'from iotoiletapp_toilet as T inner join iotoiletapp_room as R ' +\
-    'on T.room_id_id = R.id ' +\
-    'inner join iotoiletapp_roomtype as RT '+\
-    'on R.room_type_id_id = RT.id ' +\
-    'inner join iotoiletapp_floor as F ' +\
-    'on R.floor_id_id = F.id ' +\
-    'inner join iotoiletapp_toiletstatus as TS ' +\
-    'on T.id = TS.toilet_id_id ' +\
-    'inner join iotoiletapp_toiletstatustype as TST ' +\
-    'on TS.toilet_status_type_id_id = TST.id ' +\
-    'inner join iotoiletapp_sensor as S ' +\
-    'on T.id = S.toilet_id_id ' +\
-    'inner join iotoiletapp_sensordata as SD ' +\
-    'on S.id = SD.sensor_id_id')
+    with psycopg2.connect(CONN) as conn, conn.cursor() as cur:
+        sql = 'select T.toilet_name, ' +\
+        'RT.room_type_name, F.floor_name, TST.toilet_status_type_name, SD.timestamp, SD.value1 ' +\
+        'from iotoiletapp_toilet as T inner join iotoiletapp_room as R ' +\
+        'on T.room_id_id = R.id ' +\
+        'inner join iotoiletapp_roomtype as RT '+\
+        'on R.room_type_id_id = RT.id ' +\
+        'inner join iotoiletapp_floor as F ' +\
+        'on R.floor_id_id = F.id ' +\
+        'inner join iotoiletapp_toiletstatus as TS ' +\
+        'on T.id = TS.toilet_id_id ' +\
+        'inner join iotoiletapp_toiletstatustype as TST ' +\
+        'on TS.toilet_status_type_id_id = TST.id ' +\
+        'inner join iotoiletapp_sensor as S ' +\
+        'on T.id = S.toilet_id_id ' +\
+        'inner join iotoiletapp_sensordata as SD ' +\
+        'on S.id = SD.sensor_id_id'
+        cur.execute(sql, {})
+        toilet_list = cur.fetchall()
     return render(request, 'iotoiletapp/index.html', {'toilet_list': toilet_list})
     # render : 書き出し request : クライアントからのリクエスト
     # index.htmlに辞書でデータを渡す
