da3fb16c3215d1a5497f5b3d30ff6b7d3f027740
MKIMA <50836020+TomoeAsagami@users.noreply.github.com>
2019-06-12 13:40:44 +0900

    SQL文

--- iotoiletapp/views.py
+++ iotoiletapp/views.py
@@ -5,7 +5,22 @@ from iotoiletapp.forms import IotoiletappForm
 
 #一覧
 def index(request):
-    toilet_status = ToiletStatus.objects.row().order_by('floor_no') # 値を取得
+    toilet_list = CONNECTION.objects.raw('select  T.toilet_name, ' +\
+    'RT.room_type_name, F.floor_name, TST.toilet_status_type_name, SD.timestamp, SD.value1' +\
+    'from iotoiletapp_toilet as T inner join iotoiletapp_room as R ' +\
+    'on T.room_id_id = R.id ' +\
+    'inner join iotoiletapp_roomtype as RT '+\
+    'on R.room_type_id_id = RT.id ' +\
+    'inner join iotoiletapp_floor as F ' +\
+    'on R.floor_id_id = F.id ' +\
+    'inner join iotoiletapp_toiletstatus as TS ' +\
+    'on T.id = TS.toilet_id_id ' +\
+    'inner join iotoiletapp_toiletstatustype as TST ' +\
+    'on TS.toilet_status_type_id_id = TST.id ' +\
+    'inner join iotoiletapp_sensor as S ' +\
+    'on T.id = S.toilet_id_id ' +\
+    'inner join iotoiletapp_sensordata as SD ' +\
+    'on S.id = SD.sensor_id_id')
     return render(request, 'iotoiletapp/index.html', {'toilet_list': toilet_list})
     # render : 書き出し request : クライアントからのリクエスト
     # index.htmlに辞書でデータを渡す
