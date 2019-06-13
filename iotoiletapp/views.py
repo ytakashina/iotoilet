6f75ac9534bc26b9885824f5136c759600017b22
MKIMA <50836020+TomoeAsagami@users.noreply.github.com>
2019-06-13 10:15:44 +0900

    Update views.py

--- iotoiletapp/views.py
+++ iotoiletapp/views.py
@@ -7,29 +7,24 @@ CONN = "host=ytakashina.cp12crxu8zls.ap-northeast-1.rds.amazonaws.com" + \
        " port=5432 dbname=postgres user=postgres password=postgres"
 
 SQL_AVAILABLE_TOILETS = """
-select 
-    T.toilet_name,
-    RT.room_type_name,
-    F.floor_name,
-    TST.toilet_status_type_name,
-    SD.timestamp,
-    SD.value1
-from
-    iotoiletapp_toilet as T
+select floor_name, count(room_type_name='男性用' or null) as male_empty, count(room_type_name='女性用' or null) as female_empty
+
+from (
+
+select * from iotoiletapp_sensordata as SD left outer join iotoiletapp_sensor as S
+on S.id = SD.sensor_id_id
+left outer join iotoiletapp_toilet as T
+on S.toilet_id_id = T.id
 left outer join iotoiletapp_room as R
-    on T.room_id_id = R.id
+on T.room_id_id = R.id
 left outer join iotoiletapp_roomtype as RT
-    on R.room_type_id_id = RT.id
+on R.room_type_id_id = RT.id
 left outer join iotoiletapp_floor as F
-    on R.floor_id_id = F.id
-left outer join iotoiletapp_toiletstatus as TS
-    on T.id = TS.toilet_id_id
-left outer join iotoiletapp_toiletstatustype as TST
-    on TS.toilet_status_type_id_id = TST.id
-left outer join iotoiletapp_sensor as S
-    on T.id = S.toilet_id_id
-left outer join iotoiletapp_sensordata as SD
-    on S.id = SD.sensor_id_id
+on F.id = R.floor_id_id
+where (sensor_id_id, timestamp) in (select sensor_id_id, max(timestamp) from iotoiletapp_sensordata
+				group by sensor_id_id)
+) as sub
+group by floor_name;
 """
 
 
@@ -37,7 +32,15 @@ def index(request):
     with psycopg2.connect(CONN) as conn, conn.cursor() as cur:
         cur.execute(SQL_AVAILABLE_TOILETS, {})
         available_toilets = cur.fetchall()
-    available_toilets = available_toilets or [(i, i + 1, i + 2) for i in range(10)]
+        while available_toilets != None:
+            if available_toilets[5] < 3:
+                available_toilets[3] = "空"
+            elif available_toilets[5] > 3:
+                available_toilets[3] = "使用中"
+            else:
+                available_toilets[3] = "---"
+    available_toilets = available_toilets or [
+        (i, i + 1, i + 2) for i in range(10)]
     return render(request, 'iotoiletapp/index.html', {'available_toilets': available_toilets})
 
 
